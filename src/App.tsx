31f602eb1af07db0c3d5352774d58b32aecd8a38
Vivek <vivekkalp@gmail.com>
2025-02-12 15:32:19 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+   <SafeAreaView>
+    <Text>hello wold</Text>
+   </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
